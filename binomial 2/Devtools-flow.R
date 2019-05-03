d0fbee6f364774c2f603f9c5bbb1d79558f5c6a7
uzzi.valencia@berkeley.edu <uzzielvalencia@airbears2-10-142-9-196.airbears2.1918.berkeley.edu>
2019-05-03 12:29:21 -0700

    deleting binomial2

--- binomial 2/Devtools-flow.R
+++
@@ -1,9 +0,0 @@
-# =====================================================
-# Devtools workflow
-# =====================================================
-
-devtools::document()          # generate documentation
-devtools::test()              # run tests
-devtools::use_vignette()      # build vignettes
-devtools::check_man()         # checks manual
-
